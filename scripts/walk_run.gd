3fdc9ee324aa65bd8c1859735c45823bece026ba
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-11-03 22:20:25 +0530

    Walk and Run Mechanics

---
+++ scripts/walk_run.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var dir = 0
+const SPRINT_SPEED = 4
+const WALK_SPEED = 2
+var speed = WALK_SPEED
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+	
+	if Input.is_action_pressed("walk_left"):
+		position.x -= speed
+	if Input.is_action_pressed("walk_right"):
+		position.x += speed
+	
